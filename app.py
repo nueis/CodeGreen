bbdb38ccaf6fc99c58bcf5afadb7451efaf819fa
Ganghee-Lee-0522 <gh_5226@ewhain.net>
2024-11-07 13:21:22 +0900

    hotfix: 중복 파일 1차 수정

--- app.py
+++ app.py
@@ -91,5 +91,26 @@ def product_detail(product_id):
         return render_template("productDetail.html", product=product)
     return "상품을 찾을 수 없습니다.", 404
 
+@app.route('/productTest/<int:product_id>')
+def product_detail_Test(product_id):
+
+    product = {
+        'name': '토끼 키링',
+        'seller': '이화연',
+        'is_green': True,
+        'category': '이화 굿즈',
+        'price': '5,000원',
+        'short_intro': '토끼 키링',
+        'region': '서울 서대문구 이화여대길',
+        'status': '새 제품 - 최상',
+        'stock': 3,
+        'description': '수제 토끼 키링입니다',
+        'reviews': [
+            {'nickname': 'user1', 'rating': 4, 'content': '귀여워요'},
+            {'nickname': 'user2', 'rating': 5, 'content': '마음에 들어요!'}
+        ]
+    }
+    return render_template('product_detail.html', product=product)
+
 if __name__ == "__main__":
     app.run(debug=True)
