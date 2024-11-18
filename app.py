96ea0f02a1c9bc84041271e42bd2d2ae7286d632
Siwon <trxye.522@gmail.com>
2024-11-18 13:23:05 +0900

    fix: import flash in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from database import DBhandler
 import os
 import hashlib
