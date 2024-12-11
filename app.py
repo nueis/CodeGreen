23872cbc64fdc16f38df1238c25872afcd898c7f
princesschoco <90818568+princesschoco@users.noreply.github.com>
2024-12-11 17:59:57 +0900

    add like button

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, render_template, request, flash, redirect, url_for, session
+from flask import Flask, render_template, request, flash, redirect, url_for, session, jsonify
 from database import DBhandler
 import os
 import hashlib
@@ -410,7 +410,23 @@ def review_detail(review_id):
                                user=session.get('nickname'))
     return "리뷰를 찾을 수 없습니다.", 404
 
+@app.route('/show_heart/<name>/', methods=['GET'])
+def show_heart(name):
+    my_heart = DB.get_heart_byname(session['id'], name)
+    return jsonify({'my_heart': my_heart})
 
+
+@app.route('/like/<name>/', methods=['POST'])
+def like(name):
+    print(request.form)
+    my_heart = DB.update_heart(session['id'], 'Y', name)
+    return jsonify({'msg': '좋아요 완료!'})
+
+@app.route('/unlike/<name>/', methods=['POST'])
+def unlike(name):
+
+    my_heart = DB.update_heart(session['id'], 'N', name)
+    return jsonify({'msg': '좋아요 취소!'})
 
 if __name__ == "__main__":
     app.run(debug=True)
